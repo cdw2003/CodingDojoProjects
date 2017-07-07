1251ecaab44307524b21363cf5663315da400210
Cheryl Chotrani <cwchotrani@gmail.com>
2017-07-07 15:15:22 -0400

    Add files via upload

---
+++ Java/BankAccount/BankAccount.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class BankAccount{
+  protected String accountNumber;
+  protected long checkingBalance;
+  protected long savingsBalance;
+  protected static int numAccounts;
+  protected static long totalMoney;
+
+  private String getAccountNumber(){
+    Random rand = new Random();
+    ArrayList<Integer> digits = new ArrayList<Integer>();
+    for(int i = 1; i < 11; i++){
+      int randomNum = rand.nextInt(9) + 1;
+      digits.add(randomNum);
+    }
+    this.accountNumber = String.valueOf(digits.get(0));
+    for(int i = 1; i < digits.size(); i++){
+      this.accountNumber += digits.get(i);
+    }
+    System.out.println(this.accountNumber);
+    return this.accountNumber;
+  }
+
+  public BankAccount(){
+     numAccounts ++;
+     this.getAccountNumber();
+  }
+
+  public long getChecking(){
+    return this.checkingBalance;
+  }
+
+  public long getSaving(){
+    return this.savingsBalance;
+  }
+
+  public void depositMoney(int checking, int saving){
+    this.checkingBalance += checking;
+    this.savingsBalance += saving;
+    totalMoney += checking;
+    totalMoney += saving;
+    System.out.println("Your balance is now $"+ this.checkingBalance + " in checking and $"+ this.savingsBalance + " in savings." );
+  }
+
+  public void withdrawMoney(int checking, int saving){
+    this.checkingBalance -= checking;
+    this.savingsBalance -= saving;
+    totalMoney -= checking;
+    totalMoney -= checking;
+    System.out.println("Your balance is now $"+ this.checkingBalance + " in checking and $"+ this.savingsBalance + " in savings." );
+  }
+
+  public static long getTotalMoney(){
+    return totalMoney;
+  }
+}
