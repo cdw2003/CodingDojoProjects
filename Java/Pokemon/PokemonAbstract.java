90219dbbd4ea8c9df720228a047c7b359eb52362
Cheryl Chotrani <cwchotrani@gmail.com>
2017-07-08 16:59:21 -0400

    Update PokemonAbstract.java

--- Java/Pokemon/PokemonAbstract.java
+++ Java/Pokemon/PokemonAbstract.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 public abstract class PokemonAbstract implements OperatePokemon{
 
-  public createPokemon(String name, String type, int health){
-        return new Pokemon(name, type, health);
+  public Pokemon createPokemon(String name, String type, int health){
+        Pokemon pokemon = new Pokemon(name, type, health);
+        return pokemon;
   }
 
   public void attackPokemon(Pokemon pokemon){
