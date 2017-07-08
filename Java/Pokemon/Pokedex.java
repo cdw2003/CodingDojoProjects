c2712c219af59cf4fb11412abf8a08b6498a07a7
Cheryl Chotrani <cwchotrani@gmail.com>
2017-07-08 17:01:28 -0400

    Update Pokedex.java

--- Java/Pokemon/Pokedex.java
+++ Java/Pokemon/Pokedex.java
@@ -7,7 +7,7 @@ class Pokedex extends PokemonAbstract{
         info.add(pokemon.name);
         info.add(pokemon.type);
         info.add(String.valueOf(pokemon.health));
-        System.out.println("Your name is: " + pokemon.name + " Your type is: "+ pokemon.type + " You health is: " + pokemon.health);
+        System.out.println("Your name is: " + pokemon.name + " Your type is: "+ pokemon.type + " Your health is: " + pokemon.health);
         return info;
     }
   }
