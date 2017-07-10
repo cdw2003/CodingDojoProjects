b6d5c2ea436106d63c918b7ebe58f508b5834374
Cheryl Chotrani <cwchotrani@gmail.com>
2017-07-09 22:42:58 -0400

    Add files via upload

---
+++ Java/OOP/Zookeeper2/Dragon.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Dragon extends Mammal{
+  protected int energyLevel = 300;
+
+  public void displayEnergy(){
+    System.out.println("My energy level is:" + energyLevel);
+  }
+
+  public int fly(){
+    System.out.println("Arrrghhh.");
+    energyLevel -= 50;
+    return energyLevel;
+  }
+
+  public int eatHumans(){
+    System.out.println("So well. Never mind.");
+    energyLevel += 25;
+    return energyLevel;
+  }
+
+  public int attackTown(){
+    System.out.println("Whooosh.");
+    energyLevel -= 100;
+    return energyLevel;
+  }
+
+}
---
+++ Java/OOP/Zookeeper2/GorillaTest.java
@@ -0,0 +1,12 @@
+public class GorillaTest {
+  public static void main(String[] args) {
+    Gorilla a = new Gorilla();
+    a.throwFood();
+    a.throwFood();
+    a.throwFood();
+    a.eatBananas();
+    a.eatBananas();
+    a.climb();
+    a.displayEnergy();
+  }
+}
