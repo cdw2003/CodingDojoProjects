991902741ac10df3cb57a7a3bae9f1e8ef34590d
Cheryl Chotrani <cwchotrani@gmail.com>
2017-07-09 22:50:23 -0400

    Delete OperatePokemon.java

--- Java/Pokemon/OperatePokemon.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public interface OperatePokemon{
-  Pokemon createPokemon(String name, String type, int health);
-
-  void attackPokemon(Pokemon pokemon);
-
-  void pokemonInfo(Pokemon pokemon);
-}
