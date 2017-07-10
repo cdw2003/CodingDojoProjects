6e83c0b83b37a9370dabf69ed8e38407c0eed1a6
Cheryl Chotrani <cwchotrani@gmail.com>
2017-07-09 22:43:56 -0400

    Delete Mammal.java

--- Java/Zookeeper2/Mammal.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Mammal{
-  protected int energyLevel = 100;
-
-  public void displayEnergy(){
-    System.out.println("My energy level is:" + energyLevel);
-  }
-}
