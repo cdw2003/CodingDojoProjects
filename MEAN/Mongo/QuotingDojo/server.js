b9d20366f660e1dc3bf99c7701201815a8e2d7d3
Cheryl Chotrani <cwchotrani@gmail.com>
2017-09-04 10:25:14 -0400

    Add files via upload

---
+++ MEAN/Mongo/QuotingDojo/server.js
@@ -0,0 +1,49 @@
+var express = require ("express");
+var path = require("path");
+var bodyParser = require ("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, "./views"));
+app.set('view engine', 'ejs');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/quotes');
+
+var QuoteSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    quote: {type: String, required: true}
+}, {timestamps: true});
+mongoose.model('Quote', QuoteSchema);
+var Quote = mongoose.model('Quote');
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+app.post('/add_quote', function(req, res) {
+    console.log("POST DATA", req.body);
+    var quote = new Quote({name: req.body.name, quote: req.body.quote});
+    quote.save(function(err) {
+        if(err) {
+            console.log('Oops! Something went wrong');
+            res.render('quotes', {errors: quote.errors})
+        }
+        else {
+            console.log('Succesfully added a quote!')
+            res.redirect('/skip_to_quotes');
+        }
+    })
+})
+
+app.get('/skip_to_quotes', function(req, res) {
+    Quote.find({}, function(err, quotes) {
+        res.render('quotes', {quotes: quotes})
+    })
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
